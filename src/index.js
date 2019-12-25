74e120671ebd4be79c796b46680f089d57285031
Carl Froneberger <carl.froneberger@tufts.edu>
2019-12-24 19:05:54 -0500

    placing firebase config into file

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAUZgz2rm1IeYgk9Q2ws3uq0vYiH8E5HJM",
-    authDomain: "todo-35d49.firebaseapp.com",
-    databaseURL: "https://todo-35d49.firebaseio.com",
-    projectId: "todo-35d49",
-    storageBucket: "todo-35d49.appspot.com",
-    messagingSenderId: "455882528849",
-    appId: "1:455882528849:web:0fe683275a38378e328070",
-    measurementId: "G-8YGSLG05W5"
-  };
-
+import firebaseConfig from './config/firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
