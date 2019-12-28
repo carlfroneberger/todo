5f3b859af6c167e9a15c8c821e7207bcd98b6952
Carl Froneberger <carl.froneberger@tufts.edu>
2019-12-28 09:00:19 -0500

    added to do bar at top of page

--- src/MainPage/TodoPage/TodoPage.js
+++ src/MainPage/TodoPage/TodoPage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './TodoPage.css';
 import firebase from '../../firebase/firebaseFunctions';
 import Sugar from 'sugar';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 class TodoPage extends Component {
     constructor(props) {
@@ -22,6 +25,10 @@ class TodoPage extends Component {
         });
 
     }
+
+    handleAddTodo = () => {
+        // well this is where it happens...
+    }
     
     render() {
         const {name, today} = this.state;
@@ -32,7 +39,21 @@ class TodoPage extends Component {
                 <h2>Today is: {today}</h2>
                 {/* todo: make this white lol */}
                 <hr />
+
+                <InputGroup className="mb-3">
+                    <FormControl
+                    placeholder="What do you need to get done?"
+                    aria-label="What do you need to get done?"
+                    aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary">Add todo!</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+
+
             </div>
+
         );
     }
 }
