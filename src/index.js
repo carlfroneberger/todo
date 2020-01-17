8a96dd8b1c13f3d97935d992f136e11ed40ce07c
Carl Froneberger <carl.froneberger@tufts.edu>
2020-01-16 20:15:28 -0500

    resolving api key issues

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from "firebase/app";
-import firebaseConfig from './config/firebaseConfig';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let firebaseConfig = {
+    authDomain: 'todo-35d49.firebaseapp.com',
+    databaseURL: 'https://todo-35d49.firebaseio.com',
+    projectId: 'todo-35d49',
+    storageBucket: 'todo-35d49.appspot.com',
+    messagingSenderId: '455882528849',
+    appId: '1:455882528849:web:0fe683275a38378e328070',
+    measurementId: 'G-8YGSLG05W5',
+  };
+
+firebaseConfig.apiKey = process.env.REACT_APP_API;
+
 firebase.initializeApp(firebaseConfig);
+console.log(firebaseConfig);
+console.log(process.env.REACT_APP_API);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
